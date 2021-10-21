931a6a829c991fd07995fd67ad06af2af3fced60
Gabor Sornyei <info@sornyei.com>
2021-10-21 14:09:31 +0200

    Initial Commit

---
+++ Szentiras15v2/Szentiras15v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Szentiras15v2App.swift
+//  Szentiras15v2
+//
+//  Created by Gabor Sornyei on 2021. 10. 21..
+//
+
+import SwiftUI
+
+@main
+struct Szentiras15v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
