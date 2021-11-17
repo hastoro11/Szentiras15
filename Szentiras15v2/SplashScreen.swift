6ac197965e2590e812a44337a2f77e55f7fac8de
Gabor Sornyei <info@sornyei.com>
2021-11-17 08:21:43 +0100

    Splash Screen - modify

--- Szentiras15v2/SplashScreen.swift
+++ Szentiras15v2/SplashScreen.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct SplashScreen: View {
     @State var removeSplashScreen: Bool = false
-    @State var startAnimation: Bool = false
-    @State var color: Color = .accentColor
+    @State var startGlow: Bool = false
+    
     var body: some View {
         if !removeSplashScreen {
             ZStack {
@@ -21,32 +21,34 @@ struct SplashScreen: View {
                     .overlay(
                         LinearGradient(colors: [.clear, .black], startPoint: UnitPoint(x: 0.5, y: 0.2), endPoint: UnitPoint(x: 0.5, y: 2.2))
                     )
+                
                 VStack {
                     Spacer()
-                    Image("bible")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .scaledToFit()
-                        .shadow(color: Color.white.opacity(startAnimation ? 1 : 0), radius: 30)
-                        .foregroundColor(.white)
+//                    Image("bible")
+//                        .resizable()
+//                        .frame(width: 100, height: 100)
+//                        .scaledToFit()
+                    //                        .shadow(color: Color.white.opacity(startAnimation ? 1 : 0), radius: 30)
+                    //                        .foregroundColor(.white)
                     Text("Szentírás")
                         .foregroundColor(.white)
-                        .font(.custom("Zapfino", size: 42))
-                        .offset(y: -100)
+                        .font(.custom("Zapfino", size: 56))
+                        .frame(width: 300, height: 150)
+                        .glow(opacity: startGlow ? 1 : 0)
+                    Spacer()
+                    Spacer()
                     Spacer()
                     Spacer()
                 }
             }
             .onAppear {
                 withAnimation(.easeInOut(duration: 1.5)) {
-                    color = .white
-                    startAnimation = true
+                    startGlow = true
                 }
                 withAnimation(.easeInOut(duration: 1.5).delay(1.5)) {
-                    color = .accentColor
-                    startAnimation = false
+                    startGlow = false
                 }
-                DispatchQueue.main.asyncAfter(deadline: .now()+4) {
+                DispatchQueue.main.asyncAfter(deadline: .now()+3) {
                     withAnimation {
                         removeSplashScreen.toggle()
                     }
@@ -54,6 +56,21 @@ struct SplashScreen: View {
             }
         }
         
+    }
+}
+
+extension View {
+    func glow(opacity: CGFloat) -> some View {
+        self
+            .shadow(
+                color: .white.opacity(opacity),
+                radius: 10)
+            .shadow(
+                color: .white.opacity(opacity),
+                radius: 15)
+            .shadow(
+                color: .white.opacity(opacity),
+                radius: 20)
     }
 }
 
