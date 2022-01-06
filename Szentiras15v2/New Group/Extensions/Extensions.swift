ce5646f2d10d6ebb1933ec44cae452dfc7299948
Gabor Sornyei <info@sornyei.com>
2022-01-06 22:43:01 +0100

    Add UserDefaults extension

--- Szentiras15v2/New Group/Extensions/Extensions.swift
+++ Szentiras15v2/New Group/Extensions/Extensions.swift
@@ -7,8 +7,76 @@
 
 import Foundation
 
+// MARK: - String
 extension String {
     var htmlString: String {
         return self.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+    }
+}
+
+// MARK: - UserDefaults
+extension UserDefaults {
+    
+    //MARK:  Font
+    func saveFontSize(fontSize: Double) {
+        self.set(fontSize, forKey: "fontSize")
+    }
+        
+    var getFontSize: Double {
+        let size = self.double(forKey: "fontSize")
+        if size == 0 {
+            return 17
+        }
+        
+        return size
+    }
+    
+    func setFontSizeSaved(value: Bool) {
+        self.set(value, forKey: "isFontSizeSaved")
+    }
+    
+    var isFontSizeSaved: Bool {
+        self.bool(forKey: "isFontSizeSaved")
+    }
+    
+    // MARK: History
+    var historyCapacity: Int {
+        let capacity = self.integer(forKey: "historyCapacity")
+        if capacity == 0 {
+            return 10
+        }
+        
+        return capacity
+    }
+    
+    func setHistoryCapacity(to newCapacity: Int) {
+        self.set(newCapacity, forKey: "historyCapacity")
+    }
+    
+    func saveCurrent(current: Current) {
+        self.set(current.translation.id, forKey: "translationID")
+        self.set(current.book.number, forKey: "bookNumber")
+        self.set(current.chapter, forKey: "chapter")
+    }
+    
+    var savedCurrent: Current {
+        let translationID = self.integer(forKey: "translationID") == 0 ? 6 : self.integer(forKey: "translationID")
+        let bookNumber = self.integer(forKey: "bookNumber") == 0 ? 101 : self.integer(forKey: "bookNumber")
+        let chapter = self.integer(forKey: "chapter") == 0 ? 1 : self.integer(forKey: "chapter")
+        
+        let translation = Translation.get(by: translationID)
+        let book = Book.get(by: translation.id, and: bookNumber) ?? Book.default
+        
+        let current = Current(translation: translation, book: book, chapter: chapter)
+        
+        return current
+    }
+    
+    var isCurrentSaved: Bool {
+        self.bool(forKey: "isCurrentSaved")
+    }
+    
+    func setCurrentSaved(value: Bool) {
+        self.set(value, forKey: "isCurrentSaved")
     }
 }
