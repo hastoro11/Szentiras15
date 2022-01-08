b64818c3068dff872dce568eea23b44135c24f47
Gabor Sornyei <info@sornyei.com>
2022-01-08 11:08:20 +0100

    Decrease SplashScreen glow

--- Szentiras15v2/SplashScreen.swift
+++ Szentiras15v2/SplashScreen.swift
@@ -62,9 +62,6 @@ extension View {
             .shadow(
                 color: .white.opacity(opacity),
                 radius: 15)
-            .shadow(
-                color: .white.opacity(opacity),
-                radius: 20)
     }
 }
 
